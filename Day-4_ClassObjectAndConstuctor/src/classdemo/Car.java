7dd1739361ed2f53a663f62aef92dcd7eb3955fc
Harikrishna <harikrishnaderangula214@gmail.com>
2022-11-23 22:21:47 +0530

    Firstcommit

---
+++ Day-4_ClassObjectAndConstuctor/src/classdemo/Car.java
@@ -0,0 +1,23 @@
+package classdemo;
+
+public class Car {
+
+
+		
+		//default constructor
+		Car()
+		{
+			System.out.println("Default Constructor");
+		}
+		
+		
+		//data members
+		int speed;
+		void speed(int speed)
+		{
+			System.out.println("Speed of the car is: "+speed);
+		}
+
+	}
+
+
